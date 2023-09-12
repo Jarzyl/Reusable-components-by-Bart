a459656ae345836eb8803da94d71521f895bba80
barto <xhaltx4@gmail.com>
2023-09-12 20:38:13 +0200

    first commit

---
+++ TypeScript/Components/Layout.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Newsletter from "./Newsletter";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <div id="top">
+        <Navbar />
+        {children}
+      </div>
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
